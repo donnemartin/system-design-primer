f552598088c7ec25d63df40e8a218e507f4bdcf3
cclauss <cclauss@bluewin.ch>
2018-04-09 02:25:11 +0200

    Change LARGE to VehicleSize.LARGE in parking lot solution (#146)

--- solutions/object_oriented_design/parking_lot/parking_lot.py
+++ solutions/object_oriented_design/parking_lot/parking_lot.py
@@ -45,7 +45,7 @@ class Car(Vehicle):
         super(Car, self).__init__(VehicleSize.COMPACT, license_plate, spot_size=1)
 
     def can_fit_in_spot(self, spot):
-        return True if (spot.size == LARGE or spot.size == COMPACT) else False
+        return spot.size in (VehicleSize.LARGE, VehicleSize.COMPACT)
 
 
 class Bus(Vehicle):
@@ -54,7 +54,7 @@ class Bus(Vehicle):
         super(Bus, self).__init__(VehicleSize.LARGE, license_plate, spot_size=5)
 
     def can_fit_in_spot(self, spot):
-        return True if spot.size == LARGE else False
+        return spot.size == VehicleSize.LARGE
 
 
 class ParkingLot(object):
