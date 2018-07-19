65685dc8e8c922a28bb9ecf3d312a03925a752e2
cclauss <cclauss@bluewin.ch>
2018-07-19 08:09:09 +0200

    Add newlines - PEP8 style (#173)

--- solutions/system_design/query_cache/query_cache_snippets.py
+++ solutions/system_design/query_cache/query_cache_snippets.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 class QueryApi(object):
 
     def __init__(self, memory_cache, reverse_index_cluster):
--- solutions/system_design/web_crawler/web_crawler_snippets.py
+++ solutions/system_design/web_crawler/web_crawler_snippets.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 class PagesDataStore(object):
 
     def __init__(self, db):
