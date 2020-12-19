98c1d1dcb7d0eb2f1d8c95cc1b05506b537fc176
Dmitry Shvetsov <shvetsovdm@gmail.com>
2020-12-19 13:53:07 +1000

    feat: start enum val with buffer, count from top
    It is good to have buffers between enum values.
    And start counting from the "top of the hierarchy" value,
    the Director, to the bottom because it is unlikely that a
    lot of values will be added on top of the Director.

--- solutions/object_oriented_design/call_center/call_center.py
+++ solutions/object_oriented_design/call_center/call_center.py
@@ -5,10 +5,9 @@ from enum import Enum
 
 class Rank(Enum):
 
-    OPERATOR = 0
-    SUPERVISOR = 1
-    DIRECTOR = 2
-
+    OPERATOR = 30
+    SUPERVISOR = 20
+    DIRECTOR = 10
 
 class Employee(metaclass=ABCMeta):
 
